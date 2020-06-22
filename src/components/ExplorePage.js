624364d31153453cd8e82192321b0af4a75c4a53
Lime Fortytwo <melberik@gmail.com>
2020-06-22 14:09:36 +0200

    I feel done now

--- src/components/ExplorePage.js
+++ src/components/ExplorePage.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import SearchResultItem from './SearchResultItem';
 import Drinks from '../data/drinks.json'
 
